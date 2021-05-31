d6ce52e683ee7239e6927616d99dad63638ed7b1
ParkChanGyun <qkrcksrbs8@naver.com>
2021-06-01 00:11:07 +0900

    껍데기

---
+++ CoTe/src/main/java/kakao/_2020/_003/Main.java
@@ -0,0 +1,16 @@
+package kakao._2020._003;
+
+public class Main {
+ 
+    public static void main(String[] args) throws Exception {
+    	
+    }
+    
+    class Solution{
+    	
+    	
+    	
+    	
+    }
+    
+}
