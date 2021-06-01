527c84f58c15766232e191f17d719d4f559abdbd
ParkChanGyun <qkrcksrbs8@naver.com>
2021-06-01 22:44:17 +0900

    카카오 광고 삽입

--- CoTe/src/main/java/kakao/_2021/_003/Main.java
+++ CoTe/src/main/java/kakao/_2021/_003/Main.java
@@ -18,52 +18,47 @@ public class Main {
     }
     
     class Solution{
-    	List<Map<String, Integer>> FoodMaps = new ArrayList<>();
-    	int[] MaxCnt = new int[11];
-    	
-    	void comb(char[] str, int pos, StringBuilder candi) {
-    		if(pos >= str.length) {
-    			int len = candi.length();
-    			if(len >= 2) {
-    				int cnt = FoodMaps.get(len).getOrDefault(candi.toString(),0) + 1;
-    				FoodMaps.get(len).put(candi.toString(), cnt);
-    				MaxCnt[len] = Math.max(MaxCnt[len], cnt);
-    			}
-    			return;//재귀호출 종료
-    		}
-    		
-    		comb(str, pos+1, candi.append(str[pos]));
-    		candi.setLength(candi.length()-1);
-    		comb(str, pos+1, candi);
+    	int convert(String time) {
+    		String[] nums = time.split(":");
+    		return Integer.parseInt(nums[0]) * 60 * 60 +
+    			   Integer.parseInt(nums[1]) * 60 +
+    			   Integer.parseInt(nums[2]);
     	}
     	
-    	public String[] solution(String[] orders, int[] course) {
-    		//course의 최대값은 11 이하
-    		for (int i = 0; i < 11; ++i) {
-    			FoodMaps.add(new HashMap<String, Integer>());
-    		}
-    		//알파벳 순 정렬
-    		for (String str : orders) {
-    			char[] arr = str.toCharArray();
-    			Arrays.sort(arr);
-    			comb(arr, 0, new StringBuilder());
-    		}
+    	public String solution(String play_time, String adv_time, String[] logs) {
+    		int playSec = convert(play_time);
+    		int advSec = convert(adv_time);
     		
-    		List<String> list = new ArrayList<String>();
-    		for (int len : course) {
-    			for (Map.Entry<String, Integer> entry : FoodMaps.get(len).entrySet()) {
-					if(entry.getValue() >= 2 && entry.getValue() == MaxCnt[len]) {
-						list.add(entry.getKey());
-					}
+    		int[] totalSec = new int[100 * 3600];
+    		for (String log : logs) {
+    			int start = convert(log.substring(0, 8));
+    			int end = convert(log.substring(9, 17));
+    			
+    			for (int i = start; i < end; ++i) {
+    				totalSec[i] += 1;
     			}
     		}
-    		Collections.sort(list);
-    		int listSize = list.size();
-    		String[] answer = new String[listSize];
-    		for (int i = 0; i < listSize; ++i) {
-    			answer[i] = list.get(i);
+    		
+    		long currSum = 0;
+    		for (int i = 0; i < advSec; ++i) {
+    			currSum += totalSec[i];
     		}
-    		return answer;
+    		
+    		long maxSum = currSum;
+    		int maxIdx = 0;
+    		for (int i = advSec; i < playSec; ++i) {
+    			currSum = currSum + totalSec[i] - totalSec[i - advSec];
+    			if (currSum > maxSum) {
+    				maxSum = currSum;
+    				maxIdx = i - advSec +1;
+    			
+    			}
+    		}
+    		
+    		
+    		return String.format("%02d:%02d:%02d", maxIdx / 3600
+    							, maxIdx / 60 % 60
+    							, maxIdx / 60);
     	}
     }
     
